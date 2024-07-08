83ebe7662cfcc2a092313c17b240a0a18fac26b5
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2024-07-08 13:46:01 +0200

    add login system

---
+++ progressdream/ideas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path("", home, name="home"),      
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("signup/", authView, name="signup"),
+]
