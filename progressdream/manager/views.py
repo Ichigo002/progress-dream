4bb6b9fc30ce134077f2cd0d0e17fa92715a9cbb
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2024-07-08 20:09:54 +0200

    add creator

--- progressdream/manager/views.py
+++ progressdream/manager/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def hub(request):
+    return render(request, "hub.html", {})
+
+def new_project(request):
+    return render(request, "creator/new_project.html", {})
+
+def new_language(request):
+    return render(request, "creator/new_language.html", {})
+
+def new_tech(request):
+    return render(request, "creator/new_tech.html", {})
+
