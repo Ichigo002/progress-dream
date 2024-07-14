9edd987220d251542700b716f64df26d90c57670
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2024-07-14 21:13:46 +0200

    make account settings

--- progressdream/ideas/urls.py
+++ progressdream/ideas/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-    path("", home, name="home"),      
+    path("", home, name="home"),
+    path("account/", ChangePasswordView.as_view(), name="account"),
+    path("account/delete_account", delete_account, name="delete_account"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("signup/", authView, name="signup"),
 ]
--- progressdream/ideas/views.py
+++ progressdream/ideas/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
 from .models import Project
+
+class ChangePasswordView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('account')
+    template_name = 'home/account.html'
 
 @login_required
 def home(request):
@@ -11,13 +19,20 @@ def home(request):
         count.append(i)
 
     param = { "count" : count }
-    return render(request, "home.html", param)
+    return render(request, "home/home.html", param)
+
+@login_required
+def delete_account(request):
+    user = request.user
+    user.delete()
+    return redirect("home")
 
 def authView(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST or None)
         if form.is_valid():
             form.save()
+            return redirect("home")
     else:
         form = UserCreationForm()
     return render(request, "registration/signup.html", {"form" : form})
